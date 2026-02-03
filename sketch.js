eabbda7920c5af41805a44293a8f7638fdc655cd
Your Name <you@example.com>
2026-02-03 11:40:34 -0800

    added class and for loop for new bubbles

--- sketch.js
+++ sketch.js
@@ -1,7 +1,24 @@
+let bubbles = [];
+let drift;
+
 function setup() {
   createCanvas(400, 400);
+  for (let i = 0; i < 100; i++) {
+    bubbles[i] = new Bubble();
+    drift = createVector(0, 0.2)
+  }
 }
 
 function draw() {
-  background(220);
+  background(0);
+}
+
+class Bubble {
+  constructor() { // This code runs once when an instance is created.
+   this.location = createVector(random(width), random(height));  
+  }
+  show (){ // This code runs once when new Bubble is created
+
+  }
+
 }
