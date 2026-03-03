ef0604933a8c41764005941d153662e6a8968014
Your Name <you@example.com>
2026-03-03 12:38:15 -0800

    finally got bubbles to work

--- sketch.js
+++ sketch.js
@@ -1,43 +1,57 @@
 let bubbles = [];
-// let drift;
+let drift; 
 
 function setup() {
-  createCanvas(1100, 600);
-  //  //Add new bubbles over time
-  for (let i = 0; i < 100; i++) {
-    bubbles[i] = new Bubble();
-  drift = createVector(0, 0.2)
-}
+  createCanvas(1100, 800);
 }
 
 function draw() {
-//   let driftXAmount = map(mouseX, 0, width, -0.05, 0.05);
-//   let driftYAmount = map(mouseY, 0, height, 0, 0);
-//   drift.set(driftXAmount, driftYAmount);
- background(20, 30, 50); // Dark blue background
-  //  for (let i = bubbles.length - 1; i >= 0; i--) {
-  //   bubbles[i].move();
-  //   bubbles[i].show();
+  background(20, 30, 50); // Dark blue background
 
-
+  // Add new bubbles over time
+  if (frameCount % 5 == 0) {
+    bubbles.push(new Bubble());
   }
-// }
+
+  // Update and display bubbles
+  for (let i = bubbles.length - 1; i >= 0; i--) {
+    bubbles[i].move();
+    bubbles[i].show();
+    
+    // // Remove bubble if it goes off top
+    // if (bubbles[i].isOffScreen()) {
+    //   bubbles.splice(i, 1);
+    // }
+  }
+}
 
 class Bubble {
-  constructor() { // This code runs once when an instance is created.
+  constructor() {
     this.x = random(width);
-    this.y = random(height);
-    this.r = random(0,10); //radius
-    this.color = (255, 255, 255);
-// //    this.location = createVector(random(width), random(height));  
+    this.y = height + 20;
+    this.r = random(10, 30); // Radius
+    this.velocity = random(1, 3);
+    // Transparent white color
+    this.color = color(255, 255, 255, 100); 
   }
 
-  show() {  
-  // noStroke();
-  fill(this.color);
-  ellipse(this.x, this.y, this.r*2);
-    }
+  move() {
+    this.y -= this.velocity; // Move up
+    this.x += random(-.5, 0); // Slight wiggle
+    drift = createVector(0, 1)
   }
+
+  show() {
+    noStroke();
+    fill(this.color);
+    ellipse(this.x, this.y, this.r * 2);
+
+    // Add a small "highlight" for a shiny effect
+    fill(255, 255, 255, 150);
+    ellipse(this.x - this.r/3, this.y - this.r/3, this.r/2);
+  }
+}
+
    
     
   //   // ellipse(this.x - this.r/3, this.y - this.r/3, this.r/2); //found this geometry in a prompt to make a small "bubble highlight"
@@ -77,3 +91,7 @@ class Bubble {
 // then, add to Update..
 //  this.checkforParticleCollisions();
 
+//   for (let i = 0; i < 100; i++) {
+//     bubbles[i] = new Bubble();
+// }
+// drift = createVector(0, 0.2)
