34a4c6a8385c7b68da05aa1c0f4d745ec8d11458
Your Name <you@example.com>
2026-03-03 16:04:53 -0800

    added isoffscreen if statement when bubble moves to the top

--- sketch.js
+++ sketch.js
@@ -1,12 +1,13 @@
 let bubbles = [];
-let drift; 
+//let drift; 
 
 function setup() {
   createCanvas(1100, 800);
 }
 
 function draw() {
-  background(20, 30, 50); // Dark blue background
+  // background(20, 30, 50); // Dark blue background
+  background(0); //black background
 
   // Add new bubbles over time
   if (frameCount % 5 == 0) {
@@ -14,47 +15,56 @@ function draw() {
   }
 
   // Update and display bubbles
-  for (let i = bubbles.length - 1; i >= 0; i--) {
+  for (let i = bubbles.length - 1; i >= 0; i--) { //iterating the array backwards, The loop continues to run as long as the value of i is greater than or equal to 0 (the index of the first element). 
+  // When i becomes -1, the condition is false, and the loop terminates.  After each iteration of the loop, the value of i is decremented by 1,
+  //  allowing the loop to process the next element in the reverse sequence. 
     bubbles[i].move();
     bubbles[i].show();
     
-    // // Remove bubble if it goes off top
-    // if (bubbles[i].isOffScreen()) {
-    //   bubbles.splice(i, 1);
-    // }
+    // Remove bubble if it goes off top
+    if (bubbles[i].isOffScreen()) {
+      bubbles.splice(i, 1);               //splice(list, value, position), inserts a new value into the array when the object 
+    }
   }
 }
 
 class Bubble {
-  constructor() {
-    this.x = random(width);
-    this.y = height + 20;
-    this.r = random(10, 30); // Radius
-    this.velocity = random(1, 3);
+  constructor() { // This code runs once when an instance is created.
+    this.x = random(width); //width in pixels
+    this.y = height + 20; 
+    this.r = random(0,10); //radius
+    this.velocity = random(1, 3); 
     // Transparent white color
     this.color = color(255, 255, 255, 100); 
   }
-
   move() {
     this.y -= this.velocity; // Move up
     this.x += random(-.5, 0); // Slight wiggle
-    drift = createVector(0, 1)
   }
-
-  show() {
-    noStroke();
-    fill(this.color);
-    ellipse(this.x, this.y, this.r * 2);
-
-    // Add a small "highlight" for a shiny effect
-    fill(255, 255, 255, 150);
-    ellipse(this.x - this.r/3, this.y - this.r/3, this.r/2);
+  show() {  
+  noStroke();
+  fill(this.color);
+  ellipse(this.x, this.y, this.r * 2);
+  // Add a small "highlight" for a shiny effect
+  fill(255, 255, 255, 150);
+  ellipse(this.x - this.r/3, this.y - this.r/3, this.r/2);
+  }
+  isOffScreen() {
+    return this.y < -this.r * 2;
   }
 }
-
-   
     
-  //   // ellipse(this.x - this.r/3, this.y - this.r/3, this.r/2); //found this geometry in a prompt to make a small "bubble highlight"
+
+
+
+
+
+
+
+
+
+
+//   // ellipse(this.x - this.r/3, this.y - this.r/3, this.r/2); //found this geometry in a prompt to make a small "bubble highlight"
 
 // this is a hit box with a disrupt method, particle has to be in x box and y box parameters 
 // for disrupt to be called
