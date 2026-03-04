c14c0cbadc1df8de01cb9493ac2669e352e9b4d3
Your Name <you@example.com>
2026-03-03 16:32:27 -0800

    couldnt get the drift to work added notes

--- sketch.js
+++ sketch.js
@@ -1,11 +1,15 @@
 let bubbles = [];
-//let drift; 
+// let drift;                                                //for mouse location movement
 
 function setup() {
   createCanvas(1100, 800);
+  // drift = createVector(0.05, 0);
 }
 
 function draw() {
+  // let driftXAmount = map(mouseX, 0, width, -0.05, 0.05);      //copied from Jesse's particle sketch to add velocity for mouse location
+  // let driftYAmount = map(mouseY, 0, height, 0, 0);
+  // drift.set(driftXAmount, driftYAmount);                      //can't get this to work
   // background(20, 30, 50); // Dark blue background
   background(0); //black background
 
@@ -20,40 +24,47 @@ function draw() {
   //  allowing the loop to process the next element in the reverse sequence. 
     bubbles[i].move();
     bubbles[i].show();
+    // bubbles[i].update();           //Was breaking the sketch
     
     // Remove bubble if it goes off top
     if (bubbles[i].isOffScreen()) {
-      bubbles.splice(i, 1);               //splice(list, value, position), inserts a new value into the array when the object 
+      bubbles.splice(i, 1);               //splice(list, value, position), inserts a new value into the array when the object is 'off screen'
     }
   }
 }
 
 class Bubble {
   constructor() { // This code runs once when an instance is created.
-    this.x = random(width); //width in pixels
-    this.y = height + 20; 
+    this.x = random(width); //size
+    this.y = height + 20;   //size
     this.r = random(0,10); //radius
-    this.velocity = random(1, 3); 
+    this.velocity = random(1, 3); //velocity
     // Transparent white color
     this.color = color(255, 255, 255, 100); 
   }
-  move() {
-    this.y -= this.velocity; // Move up
-    this.x += random(-.5, 0); // Slight wiggle
+  move() {                    //This code runs once when .show is called. Has to be above show?
+    this.y -= this.velocity; // Move up the screen
+    this.x += random(-1, 0); // "Wiggle"s a random 
   }
-  show() {  
+  show() {                    //This code runs once when .show is called.
   noStroke();
-  fill(this.color);
-  ellipse(this.x, this.y, this.r * 2);
+  fill(this.color);             //color in constructor
+  ellipse(this.x, this.y, this.r*3); //size in constructor height, width, radius
   // Add a small "highlight" for a shiny effect
-  fill(255, 255, 255, 150);
-  ellipse(this.x - this.r/3, this.y - this.r/3, this.r/2);
+  fill(255, 255, 255, 150); //higher fill number less transparency
+  ellipse(this.x - this.r/3, this.y - this.r/3, this.r/2); //found this geometry in a prompt to make a smaller circle inside the circle
+ 
+  // update() {                 // This code runs once when .update() is called.        
+  //   this.velocity.add(drift);
+  //   this.location.add(this.velocity);                  //I thought maybe I could just add an update instance to class, but it was breaking the sketch, 
+  //                                                         is it because there is already a move instance?
+  // }
+
   }
   isOffScreen() {
-    return this.y < -this.r * 2;
-  }
+    return this.y < -this.r * 2;    //asked VS code, with if offOscreen if statement at the top
+  }    
 }
-    
 
 
 
@@ -63,9 +74,7 @@ class Bubble {
 
 
 
-
-//   // ellipse(this.x - this.r/3, this.y - this.r/3, this.r/2); //found this geometry in a prompt to make a small "bubble highlight"
-
+//   
 // this is a hit box with a disrupt method, particle has to be in x box and y box parameters 
 // for disrupt to be called
 // if(
