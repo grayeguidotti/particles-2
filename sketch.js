e33684003d6db63716c0b2c204df27def8deaa11
Your Name <you@example.com>
2026-03-20 17:31:09 -0700

    add music using play music fucntion found on yt tutorial

--- sketch.js
+++ sketch.js
@@ -1,18 +1,28 @@
 let bubbles = [];
 let drift;                                               //for mouse location movement
-let sound;
+let music;
+let soundEffect;
+let soundEffect2;
 
+//Queues music
+function preload() {
+  music = loadSound('assets/music.wav');
+  // soundEffect = loadSound('assets/sound-effect1.wav');
+  // soundEffect2 = loadSound('assets/sound-effect2.wav');
+
+}
 function setup() {
   createCanvas(1100, 800);
   // drift = createVector(0.05, 0);
+  playMusic(); 
+  //sets up music to play
 }
 
 function draw() {
   let driftXAmount = map(mouseX, 0, width, -0.01, 0.01);      //copied from Jesse's particle sketch to add velocity for mouse location
   let driftYAmount = map(mouseY, 0, height, 0, 0);
     // drift.set(driftXAmount, driftYAmount); 
-    drift = createVector(driftXAmount, driftYAmount);
-
+  drift = createVector(driftXAmount, driftYAmount);
   background(0); //black background
 
   // Add new bubbles over time
@@ -26,7 +36,7 @@ function draw() {
   //  allowing the loop to process the next element in the reverse sequence. 
     //bubbles[i].move();
     bubbles[i].show();
-    bubbles[i].update();           //Was breaking the sketch
+    bubbles[i].update();
     
     // Remove bubble if clicked
     if (bubbles[i].wasClicked()) {
@@ -37,6 +47,11 @@ function draw() {
       bubbles.splice(i, 1);
     }
   }
+}
+
+//function to play the music
+function playMusic() { 
+  music.play();
 }
 
 class Bubble {
@@ -61,24 +76,20 @@ class Bubble {
   fill(255, 255, 255, 150); //higher fill number less transparency
   ellipse(this.location.x - this.r/3, this.location.y - this.r/3, this.r/2); //found this geometry in a prompt to make a smaller circle inside the circle
   }
- 
   update() {                // This code runs once when .update() is called.        
     this.velocity.add(drift);
     this.location.add(this.velocity);                  //I thought maybe I could just add an update instance to class, but it was breaking the sketch, 
   //                                                         is it because there is already a move instance?
   }
-
   wasClicked() {
     return this.clicked;
   }
-
   checkClick() {
     let distance = dist(mouseX, mouseY, this.location.x, this.location.y);
     if (distance < this.r * 3) {  // r*3 matches your ellipse size
       this.clicked = true;
     }
   }
-
   isOffScreen() {
     return this.y < -this.r * 2;    //asked VS code, with if offOscreen if statement at the top
   }    
@@ -98,7 +109,6 @@ function mousePressed() {
 
 
 
-//   
 // this is a hit box with a disrupt method, particle has to be in x box and y box parameters 
 // for disrupt to be called
 // if(
@@ -115,9 +125,6 @@ function mousePressed() {
 //  this.velocity = p5.Vector.random2D().mult(5); 
 // }
 // the hot box and disrupt method(); would be called in update
-
-
-
 // Collide
 // add a method, make a numnerOfParticles; variable at top (let numberOfParticles = ___)
 // checkForParticleCollisions(){
